2d771a67fbe49d67db59ca555e7a63a89338872c
javiinostroza <jminostroza@uc.cl>
2022-04-21 03:50:08 -0400

    Algoritmos funcionan en consola
    require arroja problemas

---
+++ wrapper.js
@@ -0,0 +1,24 @@
+const Module = require('./allSubsetsWASM.js');
+
+Module.onRuntimeInitialized = () => {
+
+  const func = Module.cwrap('printPowerSet', null, ['number', 'number']);
+  const encoder = new TextEncoder();
+  const arr = ['a', 'b', 'c', 'd'];
+
+  const chars = arr.map(x => x.charCodeAt(0));
+  console.log(chars);
+
+  const ptrArray = new Int8Array(arr.length); 
+  
+  for (let i = 0; i < arr.length; i++)
+  {
+    ptrArray[i] = chars[i];
+  }
+  
+  arrayBuffer = Module._malloc(arr.length * ptrArray.BYTES_PER_ELEMENT);
+
+  Module.HEAP8.set(ptrArray, arrayBuffer);
+  Module._printPowerSet(arrayBuffer, arr.length);
+};
+
